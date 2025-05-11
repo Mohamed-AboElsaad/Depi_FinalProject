375c2cd1866be5b6fb80ba76c17f08f4f2ea6075
MohandSabry <119444519+MohandSabry@users.noreply.github.com>
2025-05-11 17:25:37 +0300

    Update CartTest.java

--- src/test/java/CartTest.java
+++ src/test/java/CartTest.java
@@ -3,28 +3,39 @@ import Pages.HomePage;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 
-public class CartTest extends BaseTest{
+public class CartTest extends BaseTest {
     Cart cart;
     SoftAssert softAssert;
     HomePage homePage;
 
     @Test(priority = 0)
-    public void Validate_User_Can_See_Cart(){
+    public void Validate_User_Can_See_Cart() {
         cart = new Cart(driver);
         softAssert = new SoftAssert();
         homePage = new HomePage(driver);
 
         homePage.clickOnCartPage();
         System.out.println(cart.checkCartPageTitleValue());
-        softAssert.assertEquals(cart.checkCartPageTitleValue(),"Automation Exercise - Checkout");
-       softAssert.assertTrue(cart.checkOutButtonDisplay());
-       softAssert.assertEquals(cart.checkOutButtonValue(),"Proceed To Checkout");
+        softAssert.assertEquals(cart.checkCartPageTitleValue(), "Automation Exercise - Checkout");
+        softAssert.assertTrue(cart.checkOutButtonDisplay());
+        softAssert.assertEquals(cart.checkOutButtonValue(), "Proceed To Checkout");
         cart.clickOnCheckOutCTA();
+        softAssert.assertAll();
     }
 
     @Test(priority = 1)
-    public void Validate_That_User_Can_Checkout(){
-
+    public void Validate_That_User_Can_Checkout() {
+        cart = new Cart(driver);
+        homePage = new HomePage(driver);
+        
+        cart.addOrderComment("Test order");
+        cart.clickPlaceOrder();
+        cart.enterPaymentDetails("Test User", "4111111111111111", "123", "12", "2025");
+        cart.clickPayButton();
+        
+        softAssert.assertTrue(cart.checkingDisplay(
+            By.xpath("//b[normalize-space()='Order Placed!']")), 
+            "Order success message not displayed");
+        softAssert.assertAll();
     }
-
 }
